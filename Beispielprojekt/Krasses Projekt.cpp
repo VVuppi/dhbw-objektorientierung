6b6e515648d06bbf30145aefb76382192a7dabc2
VVuppi <david_wuppermann@web.de>
2019-11-28 15:06:12 +0100

    Sehr viel verbessert

--- Beispielprojekt/Krasses Projekt.cpp
+++ Beispielprojekt/Krasses Projekt.cpp
@@ -1,4 +1,6 @@
 #include "stdafx.h"
+#include "time.h"
+#include "stdio.h"
 
 #include <Gosu/Gosu.hpp>
 #include <Gosu/AutoLink.hpp>
@@ -28,7 +30,7 @@ public:
 	int x_2=1280;											//Startwert Fortbewegung rechter Char
 	int x_2_faust = 1280;
 	int breite=30;											//breite Char
-	int breite_faust=20;
+	int breite_faust=30;
 	int hoehe_faust = 5;
 	int hoehe = 30;											//Höhe Char
 	int y_1 = 780;											//Startwert Höhe linker Char
@@ -54,12 +56,24 @@ public:
 	int hoehe_maus=10;
 	int x_maus = 960;
 	int y_maus = 540;
+	int steiflw = 0;
+	int steiflup = 0;
+	int wdhsafew = 0;
+	int wdhsafeup = 0;
+	int i = 0;
+	int j = 0;
+	int maussafe = 0;
 
 	enum kapitel {
 		menu,
 		game,
 		winner,
 	};
+
+	void delay(int milliseconds) {
+		clock_t start_time = clock();
+		while (clock() < start_time + milliseconds);
+	}
 
 	kapitel spielabschnitt = menu;
 	
@@ -76,7 +90,7 @@ public:
 				(1920 - 2*breite_end),   hoehe_end , breite_end, hoehe_end, Gosu::Color::RED, 0.0			//End Button
 			);
 			Gosu::Graphics::draw_rect(
-				x_maus, y_maus, breite_maus, hoehe_maus, Gosu::Color::GREEN, 0.0			//Cursor
+				x_maus, y_maus, breite_maus, hoehe_maus, Gosu::Color::BLUE, 0.0			//Cursor
 			);
 		};
 
@@ -88,7 +102,7 @@ public:
 				x_1, y_1, breite, hoehe, Gosu::Color::RED, 0.0			//dummy linker Char
 			);
 			Gosu::Graphics::draw_rect(
-				x_1_faust + breite - breite_faust, y_1_faust, breite_faust, hoehe_faust, Gosu::Color::RED, 0.0			//Faust linker Char
+				x_1_faust + breite - breite_faust + i, y_1_faust, breite_faust, hoehe_faust, Gosu::Color::RED, 0.0			//Faust linker Char
 			);
 			Gosu::Graphics::draw_rect(
 				x_2, 810, breite, hoehe, Gosu::Color::BLUE, 0.0			//dummy rechter Char
@@ -97,7 +111,7 @@ public:
 				x_2, y_2, breite, hoehe, Gosu::Color::BLUE, 0.0			//dummy rechter Char
 			);
 			Gosu::Graphics::draw_rect(
-				x_2_faust, y_2_faust, breite_faust, hoehe_faust, Gosu::Color::BLUE, 0.0			//Faust rechter Char
+				x_2_faust-j, y_2_faust, breite_faust, hoehe_faust, Gosu::Color::BLUE, 0.0			//Faust rechter Char
 			);
 			Gosu::Graphics::draw_rect(
 				herz_x, herz_y, 20, 20, Gosu::Color::RED, 0.0			//Herzen linker Char
@@ -148,38 +162,54 @@ public:
 				r = 255;
 				b = 0;
 			}
-			if (input().down(Gosu::MS_LEFT)) {
+			if (input().down(Gosu::KB_ESCAPE)) {
+				maussafe = 1;
 				spielabschnitt = menu;
+				
 			}
 		}
 
 		if (spielabschnitt == menu) {
+			leben_li = 3;
+			leben_re = 3;
 			x_maus = input().mouse_x();
 			y_maus = input().mouse_y();
-			if (input().down(Gosu::MS_LEFT) && (((1920 - breite_play) / 2) < x_maus)&&(x_maus < ((1920 + breite_play) / 2)) && (((1080 - hoehe_play) / 2) < y_maus)&&(y_maus <( (1080 + hoehe_play) / 2))) {
+			if (input().down(Gosu::MS_LEFT) && (((1920 - breite_play) / 2) < x_maus) && (x_maus < ((1920 + breite_play) / 2)) && (((1080 - hoehe_play) / 2) < y_maus) && (y_maus < ((1080 + hoehe_play) / 2)) && maussafe == 0) {
 				spielabschnitt = game;
 			}
-			if (input().down(Gosu::MS_LEFT) && ((1920 - 2*breite_end< x_maus) && (x_maus < (1920 - breite_end) ) && (( hoehe_end)  < y_maus) && (y_maus <( 2*hoehe_end) ))) {
+			if (input().down(Gosu::MS_LEFT) && ((1920 - 2*breite_end< x_maus) && (x_maus < (1920 - breite_end) ) && (( hoehe_end)  < y_maus) && (y_maus <( 2*hoehe_end) ))&& maussafe==0) {
 				close();
+			}
+			if (~input().down(Gosu::MS_LEFT)) {
+				maussafe = 0;
 			}
 		};
 
 		if (spielabschnitt == game) {
+			if (input().down(Gosu::MS_LEFT)) {
+				maussafe = 1;
+			}
 
 			if (leben_li <= 0 || leben_re <= 0) {
 				spielabschnitt = winner;
 			}
 
-			if (x_2_faust < (x_1 + breite)&~input().down(Gosu::KB_S)&& ~ ((x_1_faust + breite_faust) > x_2 & ~input().down(Gosu::KB_DOWN))) {				//Anfänge Hitlogik
+			if ((x_2_faust - j) < (x_1 + breite)&~input().down(Gosu::KB_S) && ~((x_1_faust + breite_faust) > x_2 & ~input().down(Gosu::KB_DOWN)) && (x_1_faust + breite_faust + i) > x_2 & ~input().down(Gosu::KB_DOWN) && ~(x_2_faust < (x_1 + breite)&~input().down(Gosu::KB_S))) {
+				x_1 = 640;
+				x_1_faust = 640;
+				x_2 = 1280;
+				x_2_faust = 1280;
+			}
+
+			if ((x_2_faust-j) < (x_1 + breite)&~input().down(Gosu::KB_S)&& ~ ((x_1_faust + breite_faust) > x_2 & ~input().down(Gosu::KB_DOWN))) {				//Anfänge Hitlogik
 				leben_li--;
 				x_1 = 640;											
 				x_1_faust = 640;
 				x_2 = 1280;											
 				x_2_faust = 1280;
-
 			}
 
-			if ((x_1_faust + breite_faust) > x_2 & ~input().down(Gosu::KB_DOWN)&& ~ (x_2_faust < (x_1 + breite)&~input().down(Gosu::KB_S))) {
+			if ((x_1_faust + breite_faust+i) > x_2 & ~input().down(Gosu::KB_DOWN)&& ~ (x_2_faust < (x_1 + breite)&~input().down(Gosu::KB_S))) {
 				leben_re--;
 				x_1 = 640;
 				x_1_faust = 640;
@@ -243,14 +273,37 @@ public:
 				y_1 = 780;
 				y_1_faust = 800;
 			}
-			if (input().down(Gosu::KB_W) & ~input().down(Gosu::KB_S)) {								//Schlaglogik linker Char
-				
-					x_1_faust = x_1 + 20;
-				
+			if (input().down(Gosu::KB_W) & ~input().down(Gosu::KB_S)) {
+				if (wdhsafew == 0 || steiflw == 1) {
+					wdhsafew = 1;
+					steiflw = 1;							//Schlaglogik linker Char
+					if (x_1_faust+i < x_1 + 21) {
+						i = i + 3;
+						if (x_1 < 1860 && x_1 < x_2 - breite - 5) {						//Bewegungslogik linker Char
+							if (input().down(Gosu::KB_D)) {
+								i = i + 5;
+							}
+						}
+						if (x_1 > 30) {
+							if (input().down(Gosu::KB_A)) {
+								i = i - 5;
+							}
+						}
+					}
+					else {
+						steiflw = 0;
+					}
+				}
+				else {
+					x_1_faust = x_1;
+					i = 0;
+				}
 			}
-				
 			else {
+				wdhsafew = 0;
+				steiflw = 0;
 				x_1_faust = x_1;
+				i = 0;
 			}
 
 
@@ -273,17 +326,46 @@ public:
 				y_2 = 780;
 				y_2_faust = 800;
 			}
-			if (input().down(Gosu::KB_UP)&~input().down(Gosu::KB_DOWN)) {								//Schlaglogik rechter Char
-				x_2_faust = x_2 - 20;
-			}
-			else {
-				x_2_faust = x_2;
-			}
+
+			if (input().down(Gosu::KB_UP) & ~input().down(Gosu::KB_DOWN)) {
+				if (wdhsafeup == 0 || steiflup == 1) {
+					wdhsafeup = 1;
+					steiflup = 1;
+					if (x_2_faust - j > x_2 - 21) {
+						j = j + 3;
+						if (x_2 < 1860) {											//Bewegungslogik rechter Char
+							if (input().down(Gosu::KB_RIGHT)) {
+								j = j - 5;
+							}
+						}
+						if (x_2 > 30 && x_2 > x_1 + breite + 5) {
+							if (input().down(Gosu::KB_LEFT)) {
+								j = j + 5;
+							}
+						}
+					}
+						else {
+							steiflup = 0;
+						}
+					}
+					else {
+						x_2_faust = x_2;
+						j = 0;
+					}
+				}
+
+				else {
+					wdhsafeup = 0;
+					steiflup = 0;
+					x_2_faust = x_2;
+					j = 0;
+				}
 		};
 
 
 	}
 };
+
 
 
 // C++ Hauptprogramm
@@ -293,3 +375,4 @@ int main()
 	window.show();
 
 }
+
